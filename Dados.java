1def57eac1b8f311092c3ac12eb92e6fdf2fcaeb
fabioWcosta <71478183+fabioWcosta@users.noreply.github.com>
2022-05-17 23:13:44 -0300

    Update Dados.java

--- Dados.java
+++ Dados.java
@@ -2,9 +2,12 @@ public class Dados{
     public static void main(String[] args) {
         int dado;
 
-        double  rolar(){
+        public int getRolar(){
             double random = Math.random();
             int rolagem = (int)(1 + random *(6-1));
+        }
+        public void setRolar() {
+            
         }
     }
 
