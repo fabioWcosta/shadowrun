7f84f40a2be600383c19596fefad959255f47f5f
fabioWcosta <71478183+fabioWcosta@users.noreply.github.com>
2022-05-18 23:59:42 -0300

    rolagens

--- Dados.java
+++ Dados.java
@@ -1,16 +1,6 @@
 public class Dados{
     public static void main(String[] args) {
-        int dado;
-        int resultado;
-
-        public int getRolar(int rolar){
-            double random = Math.random();
-            int rolagem = (int)(1 + random *(6-1));
-            //return rolagem;
-        }
-        public void setRolar() {
-            this.resultado = resultado;
-        }
+        
     }
 
 }
--- rolagens.java
+++ rolagens.java
@@ -1,4 +1,14 @@
 public class rolagens{
+    int dado;
+        int resultado;
 
-    
+        public int getRolar(int rolar){
+            double random = Math.random();
+            int rolagem = (int)(1 + random *(6-1));
+            //return rolagem;
+        }
+        public void setRolar() {
+            this.resultado = resultado;
+        }
+
 }
