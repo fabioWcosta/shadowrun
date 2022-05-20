b04e32259649a274f63d4a25bed100f75109f11a
fabioWcosta <71478183+fabioWcosta@users.noreply.github.com>
2022-05-20 00:05:09 -0300

    Update Dados.java

--- Dados.java
+++ Dados.java
@@ -1,16 +1,6 @@
 public class Dados{
     public static void main(String[] args) {
 
-        public void rolar(){
-        double random = Math.random();
-            int rolagem = (int)(1 + random *(6-1));
-            if (rolagem > 3){
-                System.out.println("Sucesso!");
-            }else{
-                System.out.println("Falha!");
-            }
-            System.out.println(rolagem);
-        }
             
     }
 
