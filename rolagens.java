9bafb3229e099550d802f5ae5843097ca33f062d
fabioWcosta <71478183+fabioWcosta@users.noreply.github.com>
2022-05-27 23:41:30 -0300

    again

--- rolagens.java
+++ rolagens.java
@@ -23,7 +23,6 @@ public class rolagens{
         System.out.println("O herói rola "+ dados +" dados!");
     }   
 
-    
         public int getRolar(int rolar){
             double random = Math.random();
             int rolagem = (int)(1 + random *(6-1));
