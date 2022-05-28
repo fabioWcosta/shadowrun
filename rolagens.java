9c34f9a7c3221bc403227ff26a8f3316eda74cdb
fabioWcosta <71478183+fabioWcosta@users.noreply.github.com>
2022-05-28 00:26:55 -0300

    vai

--- rolagens.java
+++ rolagens.java
@@ -1,39 +1,29 @@
 public class rolagens{
 
-    //atributos
-    int dados = 2;
-    double random;
-    String resultado;
-    
-    //métodos públicos
-    public void rolar(){
-        do {
-            System.out.println("Rolando dados!");
-
-        }while (dados <= dados );
-
-            random = Math.random();
-            int rolagem = (int)(1 + random *(6-1));
-            if (rolagem > 3){
-                System.out.println("Sucesso!");
-            }else{
-                System.out.println("Falha!");
-            }
-            System.out.println(rolagem);
-        }
-    public void habilidade() {
-        System.out.println("Os dados são rolados!");    
-        System.out.println("O herói rola "+ dados +" dados!");
-    }  
-       
-    //métodos especiais
-
-    public int getRolar(int rolar){
-        double random = Math.random();
-        int rolagem = (int)(1 + random *(6-1));
-        return rolagem;
-    }
-    public void setRolar(String resultado) {
-            this.resultado = resultado;
-
-}
+      //atributos
+      int dados = 2;
+      String resultado;
+      
+      //métodos públicos 
+      public void rolar(){
+          do {
+              System.out.println("Rolando dados!");
+  
+          }while (dados <= 2);
+  
+              double random = Math.random();
+              int rolagem = (int)(1 + random *(6-1));
+              if (rolagem > 3){
+                  System.out.println("Sucesso!");
+              }else{
+                  System.out.println("Falha!");
+              }
+              System.out.println("Resultado da rolagem: " +rolagem);
+         }
+      public void habilidade() {
+          System.out.println("Os dados são rolados!");    
+          System.out.println("O herói rola "+ dados +" dados!");
+      }     
+      
+  }
+  
