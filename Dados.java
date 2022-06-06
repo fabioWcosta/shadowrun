0b7a39256af24266a589abd794552c9025ec462b
fabioWcosta <71478183+fabioWcosta@users.noreply.github.com>
2022-06-05 23:55:54 -0300

    Update Dados.java

--- Dados.java
+++ Dados.java
@@ -2,11 +2,7 @@ public class Dados{
     public static void main(String[] args) {
 
     //métodos públicos 
-    private int dados;
-    public static void habilidade() {
-        System.out.println("Os dados são rolados!");    
-        System.out.println("O herói rola "+ dados +" dados!");
-    }     
+    private int dados;    
     rolar();
     
 }
