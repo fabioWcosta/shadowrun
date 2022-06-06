17b8558edea55abf9556dde4618a361b05f6923d
fabioWcosta <71478183+fabioWcosta@users.noreply.github.com>
2022-06-05 23:56:42 -0300

    Update Dados.java

--- Dados.java
+++ Dados.java
@@ -2,9 +2,15 @@ public class Dados{
     public static void main(String[] args) {
 
     //métodos públicos 
-    private int dados;   
+    //private int dados;   
     habilidade();  
     rolar();
     
 }
+
+    private static void rolar() {
+    }
+
+    private static void habilidade() {
+    }
 }
