8ea612ebb692ceea0666938171869ccd88bd7a26
fabioWcosta <71478183+fabioWcosta@users.noreply.github.com>
2022-06-05 23:52:41 -0300

    Update Dados.java

--- Dados.java
+++ Dados.java
@@ -2,21 +2,7 @@ public class Dados{
     public static void main(String[] args) {
 
     //métodos públicos 
-    public void rolar(){
-        do {
-            System.out.println("Rolando dados!");
-
-        }while (dados <= 1);
-
-            double random = Math.random();
-            int rolagem = (int)(1 + random *(6-1));
-            if (rolagem > 3){
-                System.out.println("Sucesso!");
-            }else{
-                System.out.println("Falha!");
-            }
-            System.out.println("Resultado da rolagem: " +rolagem);
-       }
+    public void rolar(
     private int dados;
     public void habilidade() {
         System.out.println("Os dados são rolados!");    
